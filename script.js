ee2da3ebe764c7a076b00baa6307fc935005ef2d
Givanildo Leite <givanildoleite@gmail.com>
2022-08-07 07:54:42 -0300

    lista de exercícios módulo 16

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function obterMedia () {
 
     document.getElementById("resultado").innerHTML = "Resultado: " + media;
 }
+
+var branco="preto"
+ var preto="cinza"
+ var cinza="branco"
+  var carro="preto"
+ var valor=30000
+ var prestacao=750
+ var entrada = 3000
+var cavalo= carro == "preto" ? "cinza" : "marron"
+    console.log(cavalo)
+
+var quantidadeDePrestacoes = parseInt(valor - entrada) / prestacao
+    console.log (quantidadeDePrestacoes)
+
+var somaDasVariaveis = "preto" + "cinza" + "branco"
+    console.log (somaDasVariaveis)
